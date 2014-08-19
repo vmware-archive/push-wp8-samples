83a0f055b276aab31523a457bfed072c1610915f
xtreme-derek-webb <dwebb@pivotallabs.com>
2014-08-19 11:26:35 -0400

    DerekW/DomenicC - [#] added App-Push partial class to show example of how to use the WP8 Push Client SDK

---
+++ PushWP8Sample/PushWP8Sample/App-Push.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MSSPush_Base.Models;
+using MSSPush_WP8;
+using MSSPush_WP8.Models;
+
+namespace PushWP8Sample
+{
+    /// <summary>
+    /// This partial class shows an example of how to Register/Unregister for Push with the 
+    /// Pivotal Mobile Services Suite Push Server using the WP8 Push Client SDK.
+    /// </summary>
+    public partial class App
+    {
+        private MSSParameters _parameters;
+
+        private MSSParameters Parameters
+        {
+            get
+            {
+                return _parameters ?? (_parameters = new MSSParameters(
+                                                        "bd9060e3-77cc-4e68-ab1a-572cb0f8a5f2",                 /* VariantUuid */
+                                                        "83c775f8-cbca-40b3-b1d9-b805dadd43c8",                 /* VariantSecret */
+                                                        "http://cfms-push-service-dev.main.vchs.cfms-apps.com", /* BaseServerUrl */
+                                                        "SampleDevice",                                         /* DeviceAlias */
+                                                        new List<string> { "Tag1", "Tag2" })                    /* Tags */);
+            }
+        }
+
+        private async void StartPushRegistration()
+        {
+            //Example of how to register for Push with the Pivotal Mobile Services Suite Push Server
+            try
+            {
+                await MSSPush.SharedInstance.RegisterForPushAsync(Parameters, OnRegistrationCompleted);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+                Debug.WriteLine("Registration verification failed.");
+            }
+        }
+
+        private async void StartPushUnregistration()
+        {
+            //Example of how to unregister for Push with the Pivotal Mobile Services Suite Push Server
+            try
+            {
+                await MSSPush.SharedInstance.UnregisterForPushAsync(Parameters, OnUnregistrationCompleted);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+                Debug.WriteLine("Unregistration verification failed.");
+            }
+        }
+
+        #region Completion Actions
+
+        private void OnRegistrationCompleted(HttpCompletionArgs args)
+        {
+            if (args.Succeeded)
+            {
+                Debug.WriteLine("Successfully registered for Push");
+
+                //HttpNotificationChannel can be accessed from the args for additional use
+                //var channel = args.RawNotificationChannel;
+                //channel.BindToShellToast();
+
+                //e.g. Unregister for Push after successfully registering
+                StartPushUnregistration();
+            }
+            else
+            {
+                Debug.WriteLine("Failed to register for Push");
+                Debug.WriteLine(args.ErrorMessage);
+            }
+        }
+
+        private void OnUnregistrationCompleted(HttpCompletionArgs args)
+        {
+            if (args.Succeeded)
+            {
+                Debug.WriteLine("Successfully unregistered for Push");
+            }
+            else
+            {
+                Debug.WriteLine("Failed to unregister for Push");
+                Debug.WriteLine(args.ErrorMessage);
+            }
+        }
+
+        #endregion
+    }
+}
--- PushWP8Sample/PushWP8Sample/MainPage.xaml.cs
+++ PushWP8Sample/PushWP8Sample/MainPage.xaml.cs
@@ -7,6 +7,8 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using MSSPush_Base.Models;
+using MSSPush_WP8;
 using PushWP8Sample.Resources;
 
 namespace PushWP8Sample
@@ -17,25 +19,6 @@ namespace PushWP8Sample
         public MainPage()
         {
             InitializeComponent();
-
-            // Sample code to localize the ApplicationBar
-            //BuildLocalizedApplicationBar();
         }
-
-        // Sample code for building a localized ApplicationBar
-        //private void BuildLocalizedApplicationBar()
-        //{
-        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
-        //    ApplicationBar = new ApplicationBar();
-
-        //    // Create a new button and set the text value to the localized string from AppResources.
-        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
-        //    appBarButton.Text = AppResources.AppBarButtonText;
-        //    ApplicationBar.Buttons.Add(appBarButton);
-
-        //    // Create a new menu item with the localized string from AppResources.
-        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
-        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
-        //}
     }
 }

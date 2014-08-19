c4f5a23ad8f5d93469a178f4ed8e766e484baaf1
xtreme-derek-webb <dwebb@pivotallabs.com>
2014-08-19 10:15:32 -0400

    DerekW/DomenicC - added project

---
+++ PushWP8Sample/PushWP8Sample/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using PushWP8Sample.Resources;
+
+namespace PushWP8Sample
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
